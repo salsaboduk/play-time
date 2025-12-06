d68ffa2931ec36737f83cdf68df02376cb75c7f7
Matthew Farrar <matthew.farrar@outlook.com>
2025-12-06 00:03:45 +0000

    Keep schema.rb checked in

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_05_141731) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_05_225105) do
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.integer "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
+  end
+
+  add_foreign_key "tasks", "lists"
 end
