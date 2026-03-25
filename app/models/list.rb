0e131e6fa1e109b64aba30740898f7919ed7019a
salsaboduk <matthew.farrar@outlook.com>
2026-03-25 23:48:18 +0000

    correct pluralisation

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,7 @@
 ## Represents a list or column on the board.
 class List < ApplicationRecord
-  has_many :task, dependent: :destroy
+  has_many :tasks, dependent: :destroy
   validates :name, presence: true
+
+  after_commit -> { broadcast }
 end
